23c6027bbab2b7fb921046497948793137050b84
kevin314 <kevin.lin.cs1@gmail.com>
2025-06-13 08:48:13 -0500

    Clean up

--- modal/pr_test.py
+++ modal/pr_test.py
@@ -17,16 +17,13 @@ def run_encoder_tests():
     """Run encoder tests on L40S GPU"""
     import subprocess
     import sys
-    import os
-    
-    os.chdir("/FastVideo")
-    
+
     command = """
     source /opt/conda/etc/profile.d/conda.sh && 
     conda activate fastvideo-dev && 
     pytest ./fastvideo/v1/tests/encoders -s
     """
-    
+
     result = subprocess.run([
         "/bin/bash", "-c", command
     ], stdout=sys.stdout, stderr=sys.stderr, check=False)
@@ -38,16 +35,13 @@ def run_vae_tests():
     """Run VAE tests on L40S GPU"""
     import subprocess
     import sys
-    import os
-    
-    os.chdir("/FastVideo")
-    
+
     command = """
     source /opt/conda/etc/profile.d/conda.sh && 
     conda activate fastvideo-dev && 
     pytest ./fastvideo/v1/tests/vaes -s
     """
-    
+
     result = subprocess.run([
         "/bin/bash", "-c", command
     ], stdout=sys.stdout, stderr=sys.stderr, check=False)
@@ -59,16 +53,13 @@ def run_transformer_tests():
     """Run transformer tests on L40S GPU"""
     import subprocess
     import sys
-    import os
-    
-    os.chdir("/FastVideo")
-    
+
     command = """
     source /opt/conda/etc/profile.d/conda.sh && 
     conda activate fastvideo-dev && 
     pytest ./fastvideo/v1/tests/transformers -s
     """
-    
+
     result = subprocess.run([
         "/bin/bash", "-c", command
     ], stdout=sys.stdout, stderr=sys.stderr, check=False)
@@ -80,16 +71,13 @@ def run_ssim_tests():
     """Run SSIM tests on 2x L40S GPUs"""
     import subprocess
     import sys
-    import os
-    
-    os.chdir("/FastVideo")
-    
+
     command = """
     source /opt/conda/etc/profile.d/conda.sh && 
     conda activate fastvideo-dev && 
     pytest ./fastvideo/v1/tests/ssim -vs
     """
-    
+
     result = subprocess.run([
         "/bin/bash", "-c", command
     ], stdout=sys.stdout, stderr=sys.stderr, check=False)
